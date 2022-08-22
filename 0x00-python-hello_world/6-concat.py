f201dfee53587f38c5dd11643be78e0ca0fe6a32
Maxwell D. Dorliea <deboy170@gmail.com>
2022-08-22 22:33:20 +0000

    Added task 6 solution

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-print(f"Welcome to {str1}", end=" ")
-print(f"{str2}!")
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"Welcome to {str1} {str2}!")
