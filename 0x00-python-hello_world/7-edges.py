c69bb0721eef858e0df403554b093d326c066c90
Maxwell D. Dorliea <deboy170@gmail.com>
2022-08-22 22:44:13 +0000

    Added task 7 solution

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-print(f"First 3 letters: {word[:3]}")
-print(f"Last 2 letters: {word[-2:]}")
-print(f"Middle word: {word[1:8]}")
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:8]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
