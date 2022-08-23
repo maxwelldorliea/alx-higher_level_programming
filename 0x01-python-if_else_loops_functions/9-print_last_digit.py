66ab5754d3ab3bc2451c772672b35bf2e9146786
Maxwell D. Dorliea <deboy170@gmail.com>
2022-08-23 15:11:11 -0700

    Bug fixed in task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -7,11 +7,7 @@ def print_last_digit(number):
     if number < 0:
         flag = 1
         number *= -1
-    if flag:
-        last = number % 10
-        last *= -1
-    else:
-        last = number % 10
+    last = number % 10
 
     print("{:d}".format(last), end="")
     return last
