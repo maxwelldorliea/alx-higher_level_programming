b9ddb75888d60a827940c2f78a88adbedde36a3a
Maxwell D. Dorliea <deboy170@gmail.com>
2022-08-23 15:56:23 -0700

    Bug fixed in task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,8 +6,8 @@ def fizzbuzz():
         if i % 5 == 0 and i % 3 == 0:
             print("FizzBuzz", end=" ")
         elif i % 3 == 0:
-            print("Fizz", end =" ");
+            print("Fizz", end=" ")
         elif i % 5 == 0:
-            print("Buzz", end =" ");
+            print("Buzz", end=" ")
         else:
             print("{:d}".format(i), end=" ")
