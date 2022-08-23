8f4021a6959286d2391daef758ce2c584b1b20d7
Maxwell D. Dorliea <deboy170@gmail.com>
2022-08-23 13:04:19 +0000

    Added task 5 solution

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(10):
-    for j in range(10):
-        if i != 9 or j != 9:
-            print("{:d}{:d}".format(i, j), end=", ")
- 
-        if i == 9 and j == 9:
-            print("{:d}{:d}".format(i, j))
+for i in range(100):
+    if i != 99:
+        print("{:02d}".format(i), end=", ")
+    else:
+        print("{:02d}".format(i))
