1a21d50ba7eda3a2a74e8fa432f27c6d8047e317
Maxwell D. Dorliea <deboy170@gmail.com>
2022-08-23 17:24:07 -0700

    Added task 6 solution

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for i in range(9):
+    for j in range(i + 1, 10):
+        if i != 8 or j != 9:
+            print("{:d}{:d}".format(i, j), end=", ")
+        else:
+            print("{:d}{:d}".format(i, j))
