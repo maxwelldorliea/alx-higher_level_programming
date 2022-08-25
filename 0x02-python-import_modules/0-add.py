cf079843b92fb2abfbcdf21b7d8d8dd720d7a15d
Maxwell D. Dorliea <deboy170@gmail.com>
2022-08-25 23:04:01 +0000

    Fixed bug in task 0 and 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-    
-if __name__ == "__main__":
-    from add_0 import add
+from add_0 import add
+
+
+def main():
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
+
+if __name__ == "__main__":
+    main()
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 
+
 def main():
     a = 10
     b = 5
@@ -8,6 +9,7 @@ def main():
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
+
 
 if __name__ == "__main__":
     main()
