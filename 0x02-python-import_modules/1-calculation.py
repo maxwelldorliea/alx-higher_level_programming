fc4a23a72b8f935fc0ade979f18b3b6e8d332767
Maxwell D. Dorliea <deboy170@gmail.com>
2022-08-25 22:51:30 +0000

    Fixed bug in task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 
-if __name__ == "__main__":
-    from calculation_1 import add, sub, mul, div
+def main():
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
+
+if __name__ == "__main__":
+    main()
