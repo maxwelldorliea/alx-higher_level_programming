156b05e21f3712698b58a21a29480d1a65f4dc08
Maxwell D. Dorliea <deboy170@gmail.com>
2022-08-25 13:48:37 +0000

    Fixed bug in task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-from add_0 import add
+import add_0
 
 a = 1
 b = 2
-res = add(a, b)
 
-print("{:d} + {:d} = {:d}".format(a, b, res))
+print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
