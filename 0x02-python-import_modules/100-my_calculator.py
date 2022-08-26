433c09228870bb77d4dfbb84b810c3d2b1c3c527
Maxwell D. Dorliea <deboy170@gmail.com>
2022-08-26 00:28:45 +0000

    Fixed bug in task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,8 +6,8 @@ from sys import argv
 def main():
     argc = len(argv) - 1
 
-    if argc == 0:
-        print("Usage: {} <a> <operator> <b>".format(argv[argc]))
+    if argc != 4:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
         exit(1)
 
     a = int(argv[1])
