31c92cc3263884722f0d542831735ce5de8e229f
Maxwell D. Dorliea <deboy170@gmail.com>
2022-08-27 22:40:29 +0000

    Added task 6 solution

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+
+    for arr in matrix:
+        for i in range(len(arr)):
+                print("{}".format(arr[i]), end="")
+                if i != len(arr) - 1:
+                    print(" ", end="")
+        print()
