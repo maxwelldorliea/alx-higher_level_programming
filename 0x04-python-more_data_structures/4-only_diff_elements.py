28b9d984dc89442d5694c4ec52fe1555161ec0e2
Maxwell D. Dorliea <deboy170@gmail.com>
2022-08-31 16:13:28 +0000

    Fixed bug in task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,11 +2,4 @@
 
 
 def only_diff_elements(set_1, set_2):
-    if not set_1 and set_2:
-        return set_2
-    if not set_2 and set_1:
-        return set_1
-    if not set_1 and not set_2:
-        return None
-
     return set_1 | set_2
