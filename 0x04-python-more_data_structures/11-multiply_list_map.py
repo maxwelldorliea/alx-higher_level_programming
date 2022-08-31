7938b1ad3a72b185d82f1479f9dda6b8ce0ee51a
Maxwell D. Dorliea <deboy170@gmail.com>
2022-08-31 16:51:20 +0000

    Fixed bug in task 11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -4,6 +4,4 @@
 def multiply_list_map(my_list=[], number=0):
     if my_list is None:
         return None
-    if not my_list:
-        return []
-    return list(map(lambda x: x * number, my_list[:]))
+    return list(map(lambda x: x * number, my_list))
