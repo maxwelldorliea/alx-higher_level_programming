bb5f6c805af872eab0d07464617acfca83e085e7
Maxwell D. Dorliea <deboy170@gmail.com>
2022-08-31 09:11:57 +0000

    Fixed bug in task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,11 +2,13 @@
 
 
 def common_elements(set_1, set_2):
+
+    if not set_1 and not set_2:
+        return None
+
     if not set_1 and set_2:
         return set_2
     if not set_2 and set_1:
         return set_1
-    if not set_1 and not set_2:
-        return None
 
     return set_1 & set_2
