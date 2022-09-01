2afc8f00b3e019ac82b5cd3ef65ece3f9f490d8c
Maxwell D. Dorliea <deboy170@gmail.com>
2022-09-01 02:04:17 +0000

    Fixed bug in task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 
 
 def roman_to_int(roman_string):
-    if roman_string is None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
     rom_to_int = {
             "I": 1,
