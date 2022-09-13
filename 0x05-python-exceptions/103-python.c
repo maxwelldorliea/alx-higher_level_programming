4f1d320357cac87f61c0f9babb5be21dd02280d9
Maxwell D. Dorliea <deboy170@gmail.com>
2022-09-13 00:40:47 +0000

    Fixed bug in task 10

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -104,6 +104,6 @@ void print_python_float(PyObject *p)
 
 	val = ((PyFloatObject *)p)->ob_fval;
 	valf = PyOS_double_to_string(val, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
-	printf("value: %s\n", valf);
+	printf("  value: %s\n", valf);
 	setbuf(stdout, NULL);
 }
