304310ea6d26622ff2cabde92dc7879f0cb4af3d
Maxwell D. Dorliea <deboy170@gmail.com>
2022-09-13 16:33:51 +0000

    Added task 3 solution

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+"""creates a new type of type  Square."""
+
+
+class Square:
+    """creates a new type of type  Square."""
+
+    def __init__(self, size=0):
+        """Initialize the square type."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Compute the area of a square."""
+        return self.__size ** 2
