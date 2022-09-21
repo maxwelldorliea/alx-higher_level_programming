ff5c973dc4de5ca81de82ad65e6417bcbffd321e
Maxwell D. Dorliea <deboy170@gmail.com>
2022-09-21 00:21:03 +0000

    Added task 8 solution

--- 0x07-python-test_driven_development/main.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-matrix_mul = __import__('100-matrix_mul').matrix_mul
-
-print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
-print(matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
-print(matrix_mul([[1.5, 2.4], [-3, -4]], [[-5, -6], [7.8, 8.7]]))
