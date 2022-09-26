5a0d82d5eb4361a29ae477c499655be7655d0b7a
Maxwell D. Dorliea <deboy170@gmail.com>
2022-09-26 18:45:46 +0000

    Added task 4 solution

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-"""Same Object or inherit from Object Module."""
+"""Only Sub Class Of Module."""
 
 
-def is_kind_of_class(obj, a_class):
-    """Check if obj is of type a_class or is instance of a_class."""
-    return isinstance(obj, a_class)
+def inherits_from(obj, a_class):
+    """Check if obj is subclass of a_class."""
+    return issubclass(type(obj), a_class)
