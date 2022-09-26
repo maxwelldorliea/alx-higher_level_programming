19dd1005176e1d03a7a96e622b5206a1925c61aa
Maxwell D. Dorliea <deboy170@gmail.com>
2022-09-26 20:11:29 +0000

    Added task 6 solution

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Geometry Module."""
+
+
+class BaseGeometry:
+    """Represent a Geometry object."""
+
+    def area(self):
+        """Compute the raise of a Geometry object."""
+        raise NotImplementedError("area() is not implemented")
