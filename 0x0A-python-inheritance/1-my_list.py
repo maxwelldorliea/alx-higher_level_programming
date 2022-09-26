053d276e53d2b2ab60dcebdbc6e07dadff4c6b33
Maxwell D. Dorliea <deboy170@gmail.com>
2022-09-26 16:19:50 +0000

    Added more test cases in task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-"""My List Module."""
+"""My List Module a custom list type."""
 
 
 class MyList(list):
-    """Represent custom list type."""
+    """Custom list type that add the functionality to print list sorted."""
 
     def print_sorted(self):
         """Print list in sorted order."""
