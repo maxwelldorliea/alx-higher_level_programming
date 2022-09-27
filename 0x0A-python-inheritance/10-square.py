17f795d9fdbbd17fa2ab93f24b0b870a14dfa1f2
Maxwell D. Dorliea <deboy170@gmail.com>
2022-09-27 01:32:53 +0000

    Added task 10 solution

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Square Module."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent Square object."""
+
+    def __init__(self, size):
+        """Initialize Square Object."""
+        self.integer_validator('size', size)
+        self.__size = size
+
+    def area(self):
+        """Compute the area of the Square."""
+        return self.__size * self.__size
