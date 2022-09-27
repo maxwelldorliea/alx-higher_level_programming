eaa9491222063b4962e913386f824f18e10eb57f
Maxwell D. Dorliea <deboy170@gmail.com>
2022-09-27 18:32:38 +0000

    Fixed bug in task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ def read_file(filename=""):
     """Read and print the content of a text to the standard output."""
     with open(file=filename, mode='r', encoding='utf-8') as f:
         for line in f:
-            print(line)
+            print(line, end="")
