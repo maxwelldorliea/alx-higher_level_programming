f993a61539cf8f15783a3943732456507182b5b8
Maxwell D. Dorliea <deboy170@gmail.com>
2022-09-27 21:33:10 +0000

    Added task 10 solution

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""File Json Module."""
+
+
+class Student:
+    """Represent Student Object."""
+
+    def __init__(self, first_name, last_name, age) -> None:
+        """Initilize Student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Convert the class to json."""
+        if type(attrs) is list and all(type(it) is str for it in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
