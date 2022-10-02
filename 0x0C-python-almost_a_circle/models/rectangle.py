3843d6bd65bfb5403a57bda197900be519a36c77
Maxwell D. Dorliea <deboy170@gmail.com>
2022-10-02 00:52:51 +0000

    Added task 2 solution

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,6 +10,22 @@ class Rectangle(base.Base):
     def __init__(self, width, height, x = 0, y= 0, id=None) -> None:
         """Initialize a reactangle object."""
         super().__init__(id)
+        if not type(width) is int:
+            raise TypeError("width must be an integer")
+        if not type(height) is int:
+            raise TypeError("height must be an integer")
+        if not type(x) is int:
+            raise TypeError("x must be an integer")
+        if not type(y) is int:
+            raise TypeError("y must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        if x < 0:
+            raise ValueError("x must be > 0")
+        if y < 0:
+            raise ValueError("y must be > 0")
         self.__width = width
         self.__height = height
         self.__x = x
@@ -23,6 +39,10 @@ class Rectangle(base.Base):
     @width.setter
     def width(self, value):
         """Set the value of the private instance field width."""
+        if not type(value) is int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -33,6 +53,10 @@ class Rectangle(base.Base):
     @height.setter
     def height(self, value):
         """Set the value of the private instance field height."""
+        if not type(value) is int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -43,6 +67,10 @@ class Rectangle(base.Base):
     @x.setter
     def x(self, value):
         """Set the value of the private instance field x."""
+        if not type(value) is int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be > 0")
         self.__x = value
 
     @property
@@ -53,4 +81,8 @@ class Rectangle(base.Base):
     @y.setter
     def y(self, value):
         """Set the value of the private instance field y."""
+        if not type(value) is int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be > 0")
         self.__y = value
