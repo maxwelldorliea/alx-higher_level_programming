ccd1a1a3c6eec9c0f89e3ba42400996984ead9f5
Maxwell D. Dorliea <deboy170@gmail.com>
2022-10-03 19:15:43 +0000

    Fixed bug in task 5

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -100,8 +100,8 @@ class Rectangle(Base):
 
     def __str__(self) -> str:
         """Return the string representation of a rectangle object."""
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - \
-    {self.__width}/{self.__height}"
+        size = f" - {self.__width}/{self.__height}"
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y}{size}"
 
     def update(self, *args, **kwargs):
         """Update rectangle using arg-var."""
