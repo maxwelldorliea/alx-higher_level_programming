b1cae49c2a2a6da57401fee2262cb7f54e2de637
Maxwell D. Dorliea <deboy170@gmail.com>
2022-11-01 18:42:18 +0000

    Added task 10 solution

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table second_table of the database hbtn_0c_0
-SELECT * FROM second_table ORDER BY score DESC, name;
+SELECT score,name FROM second_table ORDER BY score DESC;
