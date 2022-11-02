6fe295eef0ea48d84f5db5ffa7cbc331860f80d2
Maxwell D. Dorliea <deboy170@gmail.com>
2022-11-02 23:24:09 +0000

    Fixed bug in task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
 
-SELECT * FROM cities WHERE state_id = ( SELECT id FROM states WHERE name = 'California') ORDER BY cities.id;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY cities.id;
