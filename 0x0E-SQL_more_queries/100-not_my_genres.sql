c8a0cccfc941a188c69ae98819ef1b1bd523303a
Maxwell D. Dorliea <deboy170@gmail.com>
2022-11-03 02:13:51 +0000

    Added task 17 and 18 solution

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,3 +1,8 @@
 -- lists all genres of the show Dexter
 
-SELECT tv_genres.name FROM tv_genres,tv_shows, tv_show_genres WHERE tv_genres.id = tv_show_genres.genre_id AND tv_shows.id = tv_show_genres.show_id AND tv_shows.title != "Dexter" GROUP BY tv_genres.name ORDER BY tv_genres.name;
+SELECT tv_genres.name FROM tv_genres 
+where tv_genres.id NOT IN (
+SELECT tv_genres.id FROM tv_genres 
+INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter') GROUP BY tv_genres.name ORDER BY tv_genres.name;
---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,8 @@
+-- lists all not Comedy shows in the database hbtn_0d_tvshows
+SELECT tv_shows.title from tv_shows WHERE tv_shows.id NOT IN
+(
+SELECT tv_shows.title FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy")
+GROUP BY tv_shows.title ORDER BY tv_shows.title;
