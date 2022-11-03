b362ee07e1784130ea0535afa40db35216d6b740
Maxwell D. Dorliea <deboy170@gmail.com>
2022-11-03 02:18:40 +0000

    Added task 17 and 18 solution

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,5 +1,5 @@
 -- lists all not Comedy shows in the database hbtn_0d_tvshows
-SELECT tv_shows.title from tv_shows WHERE tv_shows.id NOT IN
+SELECT tv_shows.title FROM tv_shows WHERE tv_shows.id NOT IN
 (
 SELECT tv_shows.id FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
