b3264769c832319c91a7da5bce7ce8db0e72decb
Maxwell D. Dorliea <deboy170@gmail.com>
2022-11-28 21:13:24 +0000

    Added task 17 solution

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function incr () {
+  this.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
