1a5834237a7065246519d5d11a3b2cb982f14ef3
Maxwell D. Dorliea <deboy170@gmail.com>
2022-11-28 14:37:18 +0000

    Fix bug in task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-const arg = process.argv;
-
-if (arg[2]) {
-  console.log(arg[2]);
+if (process.argv[2]) {
+  console.log(process.argv[2]);
 } else {
   console.log('No argument');
 }
