759c3ef795e096380671d62db7feb5195ac71239
Maxwell D. Dorliea <deboy170@gmail.com>
2022-12-03 10:20:00 +0000

    Added task 1 solution

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ def main():
             host='localhost', port=3306, user=arg[1], passwd=arg[2], db=arg[3])
 
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id")
 
     for res in cur.fetchall():
         print(res)
