81e65396fb630e07fd8efd2c1bbbfafb4b1d33fa
Maxwell D. Dorliea <deboy170@gmail.com>
2022-12-03 10:03:16 +0000

    Fix pep warning in task 0 solution

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,12 +3,12 @@
 """Lists all states from the database hbtn_0e_0_usa"""
 
 import MySQLdb
-from sys import argv
+from sys import argv as arg
 
 
 def main():
-    conn = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
-            passwd=argv[2], db=argv[3])
+    conn = MySQLdb.connect(
+            host='localhost', port=3306, user=arg[1], passwd=arg[2], db=arg[3])
 
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id")
