d6d53163ba47e00fd0777dca6e73221d19815779
Maxwell D. Dorliea <deboy170@gmail.com>
2022-12-04 17:46:36 +0000

    Added task 6 solution

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""State Model Module."""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Maps this table to corresponding sql table."""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
