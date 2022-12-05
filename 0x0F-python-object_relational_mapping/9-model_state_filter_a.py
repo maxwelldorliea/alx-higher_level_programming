82f2c34b6fe722b90508c85206bc0ff65b3c139b
Maxwell D. Dorliea <deboy170@gmail.com>
2022-12-05 00:55:56 +0000

    Fixed bug in task 9 by replacing contains with like

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,9 +12,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     session = Session(engine)
 
-    pattern = '%a%'
     states = session.query(State).filter(
-            State.name.like(pattern)).order_by(State.id)
+            State.name.like('%a%')).order_by(State.id)
 
     for state in states:
         print(f'{state.id}: {state.name}')
