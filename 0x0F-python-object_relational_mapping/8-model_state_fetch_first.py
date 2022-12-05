1ef9578c22b5307f2247d780b7bdbd50100f58fc
Maxwell D. Dorliea <deboy170@gmail.com>
2022-12-05 00:58:21 +0000

    Fixed bug in task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,5 +15,6 @@ if __name__ == "__main__":
 
     state = session.query(State).order_by(State.id).first()
     if state:
-        print(f'{state.id}: {state.name}', end='')
-    print()
+        print(f'{state.id}: {state.name}')
+    else:
+        print('Nothing')
