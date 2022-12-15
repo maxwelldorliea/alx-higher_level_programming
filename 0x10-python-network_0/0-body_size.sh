60256b7345568563f07273fba8a236f762832d0d
Maxwell D. Dorliea <deboy170@gmail.com>
2022-12-15 18:57:32 +0000

    Fix bug in task 0 solution

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # displays the size of the body of the response
 
-curl -si $1 | grep -iF "content-length" | cut -d " " -f 2
+curl -si "$1" | grep -iF "content-length" | cut -d " " -f 2
