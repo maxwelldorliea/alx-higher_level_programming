e3fd5b578aeca8b9f11b78c32d63ee659f870a49
Maxwell D. Dorliea <deboy170@gmail.com>
2022-12-15 22:04:41 +0000

    Added task 8 Solution

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-body=$(cat $2); curl -s --header "Content-Type: application/json" -X POST -F "$body" "$1"
+body=$(cat $2); curl -s --header "Content-Type: application/json" -X POST -d "$body" "$1"
