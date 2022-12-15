bae8c912c7f9a5750a01c87f42028bd4fb66d8d3
Maxwell D. Dorliea <deboy170@gmail.com>
2022-12-15 19:45:48 +0000

    Fixed bug in task 1 solution

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to the URL, and displays the body of the response
-sd=$(curl -si -o /dev/NULL -w "%{http_code}" "$1"); if [[ $sd -eq "200" ]]; then curl -s "$1"; fi
+sd=$(curl -sLi -o /dev/NULL -w "%{http_code}" "$1"); if [[ $sd -eq "200" ]]; then curl -s "$1"; fi
