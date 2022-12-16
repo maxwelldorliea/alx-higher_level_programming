5b5add577457254da5eeec4f9d2a3c7e3bb3332b
Maxwell D. Dorliea <deboy170@gmail.com>
2022-12-16 18:53:26 +0000

    Fixed bug in task 5 solution

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,7 +8,7 @@ def main() -> None:
     """Send a request to the URL and displays the value of the."""
     """Variable X-Request-Id in the response header."""
     res = requests.get(sys.argv[1])
-    header = res.headers['X-Request-Id']
+    header = res.headers.get('X-Request-Id')
     print(header)
 
 
