606a07719d809c7ed1113476d5118947b95e6b43
Maxwell D. Dorliea <deboy170@gmail.com>
2022-12-16 18:43:23 +0000

    Fixed bug in task 0 solution

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,11 +8,11 @@ def main():
     """Fetch https://alx-intranet.hbtn.io/status."""
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         byte = res.read()
-        content = f"- content: {byte}"
-        tc_type = f"- type: {type(content)}"
-        utf8 = f"- utf8 content: {byte.decode()}"
-        output = "Body response:\n\t{}\n\t{}\n\t{}".format(
-                content, tc_type, utf8)
+        content = f"content: {byte}"
+        tc_type = f"type: {type(byte)}"
+        utf8 = f"utf8 content: {byte.decode()}"
+        output = "Body response:\n\t- {}\n\t- {}\n\t- {}".format(
+                tc_type, content, utf8)
 
     print(output)
 
