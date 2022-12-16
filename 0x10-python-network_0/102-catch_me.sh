67b2d8e23118b832e6ac673435feec871897d636
Maxwell D. Dorliea <deboy170@gmail.com>
2022-12-16 01:57:39 +0000

    Fixed bug in task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -sL -H "X-School-User-Id: 98" -X PUT 0.0.0.0:5000/catch_me
+curl -sL -H "origin: HolbertonSchool" -X PUT 0.0.0.0:5000/catch_me_3
