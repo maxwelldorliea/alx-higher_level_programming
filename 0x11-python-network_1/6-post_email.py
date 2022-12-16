7c250559f9946729fce1022c9074b66424a11191
Maxwell D. Dorliea <deboy170@gmail.com>
2022-12-16 19:02:49 +0000

    Fixed bug in task 6 solution

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,7 +8,7 @@ def main() -> None:
     """Send a POST request to the passed URL with the email as a."""
     """Parameter, and finally displays the body of the response."""
     data = {'email': sys.argv[2]}
-    res = requests.post(sys.argv[1], data)
+    res = requests.post(sys.argv[1], data=data)
     print('Your email is:', res.text)
 
 
