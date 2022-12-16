c1998920ccaeb9dd96431c3fdff4003b97eecdc5
Maxwell D. Dorliea <deboy170@gmail.com>
2022-12-16 01:39:33 +0000

    Fixed bug in task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,10 +4,17 @@
 def find_peak(arr):
     """finds a peak in a list of unsorted integer."""
     l = 0
-    r = len(arr)
+    size = len(arr)
+    r = size - 1
 
     if not arr:
         return
+    if size == 1:
+        return arr[0]
+    if arr[0] >= arr[1]:
+        return arr[0]
+    if arr[size - 1] >= arr[size - 2]:
+        return arr[size - 1]
     while l < r:
         m = l + (r - l) // 2
 
