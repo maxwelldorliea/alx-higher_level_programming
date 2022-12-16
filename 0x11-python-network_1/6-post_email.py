591315ca54c6f2d819a638251970a07a1330c7b5
Maxwell D. Dorliea <deboy170@gmail.com>
2022-12-16 18:59:22 +0000

    Added task 6 solution

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Request ALX SE MOdule."""
+import requests
+import sys
+
+
+def main() -> None:
+    """Sends a POST request to the passed URL with the email as a."""
+    """Parameter, and finally displays the body of the response."""
+    data = {'email': sys.argv[2]}
+    res = requests.post(sys.argv[1], data)
+    print('Your email is:', res.text)
+
+
+if __name__ == '__main__':
+    main()
