c31bbdfc2ae587976713cdb4896e54a0627135c9
Maxwell D. Dorliea <deboy170@gmail.com>
2023-01-17 20:30:14 +0000

    Fixed bug in task 4 solution

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,8 +10,8 @@ request(argv[2], (err, res, body) => {
   const data = JSON.parse(body).results;
   let count = 0;
   // data = data.filter(({ characters }) => characters.includes('https://swapi-api.alx-tools.com/api/people/18/'));
-  for (const obj of data) {
-    if (obj.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+  for (let i = 0; data[i] !== undefined; i++) {
+    if (data[i].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
       count++;
     }
   }
