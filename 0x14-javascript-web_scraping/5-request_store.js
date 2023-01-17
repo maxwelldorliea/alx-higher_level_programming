b5e4a13a961d31a05b73080fa220c7e16a75b2a0
Maxwell D. Dorliea <deboy170@gmail.com>
2023-01-17 14:51:57 +0000

    Added task 5 solution

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs')
+const argv = require('process').argv;
+
+request(argv[2], (err, res, body) => {
+  if (err) {
+    throw err;
+  }
+  fs.writeFile(argv[3], body, 'utf8', (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+});
