9caf5c5229698bb2f87642aa3dccd6b9830b35bd
Maxwell D. Dorliea <deboy170@gmail.com>
2023-01-17 13:21:21 +0000

    Added task 4 solution

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const argv = require('process').argv;
+
+request(argv[2], (err, res, body) => {
+  if (err) {
+    throw err;
+  }
+  let data = JSON.parse(body).results;
+  data = data.filter(({ characters }) => characters.includes('https://swapi-api.alx-tools.com/api/people/18/'));
+  console.log(data.length);
+});
