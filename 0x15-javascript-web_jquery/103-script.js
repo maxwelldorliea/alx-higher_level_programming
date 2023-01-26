c51be5422012e6ceadd68cdc0c565cfc3e65fdcf
Maxwell D. Dorliea <deboy170@gmail.com>
2023-01-26 02:13:34 +0000

    Added task 13 solution

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -6,4 +6,12 @@ $(function () {
       $('#hello').text(data.hello);
     });
   });
+  $('#language_code').keyup(function (e) {
+    if (e.keyCode === 13) {
+      const CODE = $('#language_code').val();
+      $.get(URL + CODE, function (data, stateCode) {
+        $('#hello').text(data.hello);
+      });
+    }
+  });
 });
